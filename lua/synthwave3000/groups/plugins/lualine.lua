78d4417716dcd072d16c7f8bcc3a5f520c06e860
web-dev-codi <cordiscobrian@gmail.com>
2026-05-09 16:57:42 +0200

    ui: updated transpanency

--- lua/synthwave3000/groups/plugins/lualine.lua
+++ lua/synthwave3000/groups/plugins/lualine.lua
@@ -1,27 +1,25 @@
 local M = {}
 
 function M.build(p, o)
-	local bg = o.transparent and "NONE" or (o.background or p.bg)
-	local bg_darker = o.transparent and "NONE" or p.bg_darker
 	local function mode(fg, bg)
 		return {
 			a = { fg = fg, bg = bg, gui = "bold" },
 			b = { fg = p.fg, bg = p.bg_highlight },
-			c = { fg = p.fg, bg = bg },
+			c = { fg = p.fg, bg = p.bg_dark },
 		}
 	end
 
 	return {
-		normal = mode(bg, p.pink),
-		insert = mode(bg, p.cyan),
-		visual = mode(bg, p.purple),
-		replace = mode(bg, p.orange),
-		command = mode(bg, p.yellow),
-		terminal = mode(bg, p.green),
+		normal = mode(p.bg, p.pink),
+		insert = mode(p.bg, p.cyan),
+		visual = mode(p.bg, p.purple),
+		replace = mode(p.bg, p.orange),
+		command = mode(p.bg, p.yellow),
+		terminal = mode(p.bg, p.green),
 		inactive = {
-			a = { fg = p.fg_dim, bg = bg_darker, gui = "bold" },
-			b = { fg = p.fg_dim, bg = bg_darker },
-			c = { fg = p.fg_dim, bg = bg_darker },
+			a = { fg = p.fg_dim, bg = p.bg_darker, gui = "bold" },
+			b = { fg = p.fg_dim, bg = p.bg_darker },
+			c = { fg = p.fg_dim, bg = p.bg_darker },
 		},
 	}
 end
